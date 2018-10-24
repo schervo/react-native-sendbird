960feb17da8c6245044669154b43fce1d6f5aa9b
Sebastian Chervo <schervo10@gmail.com>
2018-10-24 12:18:28 -0300

    sendbird

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import SendBird from 'sendbird'
 import AppNavigator from './navigation/navigation'
 import configureStore from './redux/store'
-/* eslint-disable-next-line */
-class App extends Component { 
+// import SendBird from './helpers/sendbird'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    new SendBird({
+      appId: '6102229F-0A53-417E-841A-A0F2841663E4',
+    })
+  }
+
   render() {
     return (
       <Provider store={configureStore}>
